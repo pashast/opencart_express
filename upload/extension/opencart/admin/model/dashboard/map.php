afed613c3cca471c2b6b04edd749ff812ba469a8
pashast <pasha.strus@gmail.com>
2023-04-21 13:39:00 +0300

    resolve conflict

--- upload/extension/opencart/admin/model/dashboard/map.php
+++ upload/extension/opencart/admin/model/dashboard/map.php
@@ -11,12 +11,8 @@ class Map extends \Opencart\System\Engine\Model {
 		}
 
 		if ($implode) {
-			if ($this->config->get('config_checkout_payment_address')) {
-				$query = $this->db->query("SELECT COUNT(*) AS `total`, SUM(o.`total`) AS `amount`, c.`iso_code_2` FROM `" . DB_PREFIX . "order` o LEFT JOIN `" . DB_PREFIX . "country` c ON (o.`payment_country_id` = c.`country_id`) WHERE o.`order_status_id` IN(" . implode(',', $implode) . ") GROUP BY o.`payment_country_id`");
-			} else {
-				$query = $this->db->query("SELECT COUNT(*) AS `total`, SUM(o.`total`) AS `amount`, c.`iso_code_2` FROM `" . DB_PREFIX . "order` o LEFT JOIN `" . DB_PREFIX . "country` c ON (o.`shipping_country_id` = c.`country_id`) WHERE o.`order_status_id` IN(" . implode(',', $implode) . ") GROUP BY o.`shipping_country_id`");
-			}
-			
+			$query = $this->db->query("SELECT COUNT(*) AS `total`, SUM(o.`total`) AS `amount`, c.`iso_code_2` FROM `" . DB_PREFIX . "order` o LEFT JOIN `" . DB_PREFIX . "country` c ON (o.`payment_country_id` = c.`country_id`) WHERE o.`order_status_id` IN(" . implode(',', $implode) . ") AND o.`payment_country_id` != '0' GROUP BY o.`payment_country_id`");
+
 			return $query->rows;
 		} else {
 			return [];
